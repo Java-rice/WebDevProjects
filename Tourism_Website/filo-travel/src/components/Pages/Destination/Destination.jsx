26e72191a8159faf4b9c730e183801ea07f66172
Java-rice <johnmark.p.peroche@gmail.com>
2024-05-26 16:18:09 +0800

    about and npm routing

---
+++ Tourism_Website/filo-travel/src/components/Pages/Destination/Destination.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Destination = () => {
+    return (
+        <div>
+            <h1>Destination</h1>
+            <p>This is the Destination page content.</p>
+        </div>
+    );
+};
+
+export default Destination;
