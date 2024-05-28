d61bf43fbfc8289ef3233b0ebc815a4841614ac6
Java-rice <johnmark.p.peroche@gmail.com>
2024-05-28 22:50:13 +0800

    done transition of pages

--- Tourism_Website/filo-travel/src/components/TransitionWrapper.jsx
+++ Tourism_Website/filo-travel/src/components/TransitionWrapper.jsx
@@ -1,22 +1,24 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+// src/components/TransitionWrapper.js
+import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { useLocation } from "react-router-dom";
+import "./TransitionWrapper.css";
 
 const TransitionWrapper = ({ children }) => {
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        document.body.classList.add('fade-in');
-
-        return () => {
-            document.body.classList.remove('fade-in');
-        };
-    }, [location]);
-
-    return (
-        <div className="transition-wrapper">
-            {children}
-        </div>
-    );
+  return (
+    <TransitionGroup component={null}>
+      <CSSTransition
+        key={location.key}
+        timeout={300}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div className="transition-wrapper">{children}</div>
+      </CSSTransition>
+    </TransitionGroup>
+  );
 };
 
 export default TransitionWrapper;
