c8bd89968317d8fe5f66c12251e2b19ca4686e49
John Patrick Lagatuz <patrick.zutagal@gmail.com>
2024-05-28 18:35:29 +0800

    updated carousel with bg

--- Tourism_Website/filo-travel/src/components/Pages/Home/Carousel/Carousel.jsx
+++ Tourism_Website/filo-travel/src/components/Pages/Home/Carousel/Carousel.jsx
@@ -17,74 +17,83 @@ const Carousel = () => {
 
   return (
     <div className="carousel-container">
-      <Slider {...settings}>
-        <div>
-          <img
-            src={boracay_img}
-            alt="White Beach, Boracay"
-            className="carousel-image"
-          />
+      <div className="carousel-background">
+        <img
+          src={destination_bg}
+          alt="Destination Background"
+          className="carousel-image"
+        />
+        <div className="carousel-slider">
+          <Slider {...settings}>
+            <div>
+              <img
+                src={boracay_img}
+                alt="White Beach, Boracay"
+                className="carousel-image"
+              />
+            </div>
+            <div>
+              <img
+                src={destination_bg}
+                alt="Chocolate Hills, Bohol"
+                className="carousel-image"
+              />
+            </div>
+            <div>
+              <img
+                src={boracay_img}
+                alt="Puerto Princesa Underground River, Palawan"
+                className="carousel-image"
+              />
+            </div>
+            <div>
+              <img
+                src={boracay_img}
+                alt="Cloud 9, Siargao"
+                className="carousel-image"
+              />
+            </div>
+            <div>
+              <img
+                src={boracay_img}
+                alt="Cebu Beaches and Diving Spots"
+                className="carousel-image"
+              />
+            </div>
+            <div>
+              <img
+                src={boracay_img}
+                alt="Banaue Rice Terraces, Ifugao"
+                className="carousel-image"
+              />
+            </div>
+            <div>
+              <img
+                src={boracay_img}
+                alt="Rizal Park, Manila"
+                className="carousel-image"
+              />
+            </div>
+            <div>
+              <img
+                src={boracay_img}
+                alt="Kayangan Lake"
+                className="carousel-image"
+              />
+            </div>
+            <div>
+              <img
+                src={boracay_img}
+                alt="Taal Volcano, Batangas"
+                className="carousel-image"
+              />
+            </div>
+            <div>
+              <img src={boracay_img} alt="Slide 3" className="carousel-image" />
+            </div>
+          </Slider>
         </div>
-        <div>
-          <img
-            src={destination_bg}
-            alt="Chocolate Hills, Bohol"
-            className="carousel-image"
-          />
-        </div>
-        <div>
-          <img
-            src={boracay_img}
-            alt="Puerto Princesa Underground River, Palawan"
-            className="carousel-image"
-          />
-        </div>
-        <div>
-          <img
-            src={boracay_img}
-            alt="Cloud 9, Siargao"
-            className="carousel-image"
-          />
-        </div>
-        <div>
-          <img
-            src={boracay_img}
-            alt="Cebu Beaches and Diving Spots"
-            className="carousel-image"
-          />
-        </div>
-        <div>
-          <img
-            src={boracay_img}
-            alt="Banaue Rice Terraces, Ifugao"
-            className="carousel-image"
-          />
-        </div>
-        <div>
-          <img
-            src={boracay_img}
-            alt="Rizal Park, Manila"
-            className="carousel-image"
-          />
-        </div>
-        <div>
-          <img
-            src={boracay_img}
-            alt="Kayangan Lake"
-            className="carousel-image"
-          />
-        </div>
-        <div>
-          <img
-            src={boracay_img}
-            alt="Taal Volcano, Batangas"
-            className="carousel-image"
-          />
-        </div>
-        <div>
-          <img src={boracay_img} alt="Slide 3" className="carousel-image" />
-        </div>
-      </Slider>
+      </div>
     </div>
   );
 };
