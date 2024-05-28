3d9e901ab58ea261a97c0931c00ccd7c6f42a5ff
John Patrick Lagatuz <patrick.zutagal@gmail.com>
2024-05-28 18:53:04 +0800

    carousel with bg 5 images

--- Tourism_Website/filo-travel/src/components/Pages/Home/Carousel/Carousel.jsx
+++ Tourism_Website/filo-travel/src/components/Pages/Home/Carousel/Carousel.jsx
@@ -9,7 +9,7 @@ const Carousel = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
